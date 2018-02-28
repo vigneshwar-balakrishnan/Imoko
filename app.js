9e93d23c7a2aaa0fd40df483af0a13b283db9322
Ben Sooraj <bensoorajm@gmail.com>
2018-02-28 13:47:39 +0530

    setup route skeleton

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const port = Number(process.env.PORT || 3000);
+
+// Middlewares
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+
+// For testing, sending a formatted response to the browser
+app.set('json spaces', 2)
+
+// Import all routes
+const postRoutes = require('./routes/postRoutes');
+const uploadRoutes = require('./routes/uploadRoutes');
+
+// Assign the above routes to route paths
+app.use('/', uploadRoutes);
+app.use('/gallery', postRoutes);
+
+app.listen(port);
